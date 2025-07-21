000a86095c56b33839c4e1ea9ea5d18fd457ee69
PaolaVitalMente <kahudigital2023@gmail.com>
2025-07-21 13:01:04 -0500

    feat: Add multimedia resources with thumbnails + exercise category

--- app/page.tsx
+++ app/page.tsx
@@ -2196,11 +2196,6 @@ Gracias!`
             </div>
           </div>
         )}
-                </div>
-              </div>
-            </div>
-          </div>
-        )}
 
         {showSupplementDialog && (
           <div className="fixed inset-0 bg-gray-600 bg-opacity-50 overflow-y-auto h-full w-full z-50">
